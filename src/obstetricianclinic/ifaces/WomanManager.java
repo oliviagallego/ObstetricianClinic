4e5ba9e18278852c56b859de8c63971f827585ce
nicos <nicos@10.60.59.107>
2024-04-24 17:40:24 +0200

    c

--- src/obstetricianclinic/ifaces/WomanManager.java
+++ src/obstetricianclinic/ifaces/WomanManager.java
@@ -6,6 +6,6 @@ import java.util.List;
 public interface WomanManager {
 	public void resgisterWoman(Woman woman);
 	public void deleteWoman(int id);
-	public List<Woman> searchWomanByNameAndSurname(String name,String surname); // Otra forma de hacer --> public void viewWoman(Woman[] womansList, int id); 
+	public List<Woman> searchWomanByNameAndSurname(int id); // Otra forma de hacer --> public void viewWoman(Woman[] womansList, int id); 
 	public void updateWoman (Woman woman);
 }
