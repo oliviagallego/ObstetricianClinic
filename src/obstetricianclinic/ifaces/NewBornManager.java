4a4b2c187d19a4a019a6ba793db68a3a28dfaab0
Laura Gallego <laura.gallegoparedes@usp.ceu.es>
2024-04-27 18:04:45 +0200

    I've made some changes to the interfaces

--- src/obstetricianclinic/ifaces/NewBornManager.java
+++ src/obstetricianclinic/ifaces/NewBornManager.java
@@ -3,6 +3,6 @@ package obstetricianclinic.ifaces;
 import obstetricianclinic.pojos.*;
 
 public interface NewBornManager {
-	public void addNewBorn(String name, String surname, float weight, Gender gender);
+	public void addNewBorn(String name, String surname, float weight, String gender);
 	public void addNewBornReport(String text, Newborn newborn);
 }
