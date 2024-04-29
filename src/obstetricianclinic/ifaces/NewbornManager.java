06080a24edcf8f0d17421ee48c6fa069b0e12b70
Jaime Martin <jaime.martincasado@usp.ceu.es>
2024-04-29 17:20:09 +0200

    commit

---
+++ src/obstetricianclinic/ifaces/NewbornManager.java
@@ -0,0 +1,8 @@
+package obstetricianclinic.ifaces;
+
+import obstetricianclinic.pojos.*;
+
+public interface NewbornManager {
+		public void addNewBorn(String name, String surname, float weight, String gender);
+	}
+
