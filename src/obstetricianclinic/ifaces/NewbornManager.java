43989b97a18a877fc948f61369df63be6969ab64
USUARIO <USUARIO@10.60.59.151>
2024-04-29 17:17:30 +0200

    .

--- src/obstetricianclinic/ifaces/NewbornManager.java
+++
@@ -1,7 +0,0 @@
-package obstetricianclinic.ifaces;
-
-import obstetricianclinic.pojos.*;
-
-public interface NewbornManager {
-	public void addNewBorn(String name, String surname, float weight, String gender);
-}
