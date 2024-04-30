4c5e572dde851abf0d05b292383a228c67a688ed
Jaime Martin <jaime.martincasado@usp.ceu.es>
2024-04-30 16:45:05 +0200

    jaime

--- src/obstetricianclinic/ifaces/ObstetricianManager.java
+++ src/obstetricianclinic/ifaces/ObstetricianManager.java
@@ -1,7 +1,8 @@
 package obstetricianclinic.ifaces;
+import obstetricianclinic.pojos.*;
 
 public interface ObstetricianManager {
-	public void addObstetrician(String name, String specialization);
+	public void addObstetrician(Obstetrician obstetrician);
     public void removeObstetrician(int id);
     public void updateObstetrician(int id, String newName, String newSpecialization);
 }
--- src/obstetricianclinic/jdbc/JDBCNewbornManager.java
+++ src/obstetricianclinic/jdbc/JDBCNewbornManager.java
@@ -1,8 +1,11 @@
 package obstetricianclinic.jdbc;
 
 import java.sql.Connection;
-
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import obstetricianclinic.pojos.Newborn;
 import obstetricianclinic.ifaces.NewbornManager;
+import java.sql.Date;
 
 public class JDBCNewbornManager implements NewbornManager {
 	private ConnectionManager conMan;
@@ -14,8 +17,21 @@ public class JDBCNewbornManager implements NewbornManager {
 	}
 
 	@Override
-	public void addNewBorn(String name, String surname, float weight, String gender) {
-		// TODO Auto-generated method stub
+	public void addNewborn(Newborn newborn) {
+		try {
+			String query = "INSERT INTO Newborns (name, surname, dob, weight, gender) VALUES (?, ?, ?, ?, ?);";
+			PreparedStatement insert = c.prepareStatement(query);
+			insert.setString(1, newborn.getName());
+			insert.setString(2, newborn.getSurname());
+			insert.setFloat(3, newborn.getWeight());
+			insert.setString(4, newborn.getGender());
+			insert.setDate(5, newborn.getDob());
+			insert.executeUpdate();
+			insert.close();
+		}catch(SQLException sqlE) {
+			System.out.println("Database exception");
+			sqlE.printStackTrace();
+		}
 
 	}
 
