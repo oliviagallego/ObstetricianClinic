5e2484fb0aafe80493844336b077f9776a29fe7b
USUARIO <USUARIO@10.60.58.187>
2024-05-02 19:08:40 +0200

    cambios jdbc

---
+++ src/obstetricianclinic/jdbc/JDBCLabStaffManager.java
@@ -0,0 +1,41 @@
+package obstetricianclinic.jdbc;
+
+import java.util.List;
+
+import obstetricianclinic.ifaces.LabStaffManager;
+import obstetricianclinic.pojos.LabReport;
+import obstetricianclinic.pojos.LaboratoryStaff;
+
+public class JDBCLabStaffManager implements LabStaffManager {
+
+	@Override
+	public void addLabReport(LabReport report) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public LabReport getLabReport(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateLabReport(LabReport report) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteLabReport(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<LabReport> listAllLabReports(LaboratoryStaff staff) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
