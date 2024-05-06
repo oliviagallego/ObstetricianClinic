bd7166c8bcbec5382fe6af1eb4c94941dba424b3
USUARIO <USUARIO@LAPTOP-ROS8RHM6>
2024-05-06 17:04:57 +0200

    quitamos drug

--- src/obstetricianclinic/pojos/Disease.java
+++ src/obstetricianclinic/pojos/Disease.java
@@ -11,13 +11,11 @@ public class Disease implements Serializable{
 	private static final long serialVersionUID = -1079965302432167439L;
 	private Integer id;
     private String diseaseType;
-    private String drug;
     private List<Woman> women;
     
     public Disease() {
     	super();
     	this.women = new ArrayList<Woman>();
-    	this.drug = assignDrugBasedOnDiseaseType(this.diseaseType);
     }
     
 	public Disease(Integer id, String diseaseType) {
@@ -25,7 +23,6 @@ public class Disease implements Serializable{
 		this.id = id;
 		this.diseaseType = diseaseType;
 		this.women = new ArrayList<Woman>();
-		this.drug = assignDrugBasedOnDiseaseType(this.diseaseType);
 	}
 
 	public Integer getId() {
@@ -51,14 +48,11 @@ public class Disease implements Serializable{
 	public void setWomen(List<Woman> women) {
 		this.women = women;
 	}
-	public String getDrug() {
-		return drug;
-	}
 	
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(diseaseType, drug, id, women);
+		return Objects.hash(diseaseType,id, women);
 	}
 
 	@Override
@@ -70,28 +64,12 @@ public class Disease implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Disease other = (Disease) obj;
-		return Objects.equals(diseaseType, other.diseaseType) && Objects.equals(drug, other.drug)
+		return Objects.equals(diseaseType, other.diseaseType)
 				&& Objects.equals(id, other.id) && Objects.equals(women, other.women);
 	}
 
 	@Override
 	public String toString() {
-		return "Disease [id=" + id + ", diseaseType=" + diseaseType + ", drug=" + drug + ", women=" + women + "]";
+		return "Disease [id=" + id + ", diseaseType=" + diseaseType+ ", women=" + women + "]";
 	}
-
-	private String assignDrugBasedOnDiseaseType(String diseaseType) {
-        if (diseaseType == null) {
-            return "";
-        }
-        switch (diseaseType.toLowerCase()) {
-            case "anemia":
-                return "Paracetamol and Fisiogen";
-            case "hypothyroidism":
-                return "Enantyum";
-            case "urinary tract infection":
-                return "Monurol";
-            default:
-                return "No drug assigned";
-        }
-    }
 }
