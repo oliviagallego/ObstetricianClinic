acf53261724dfd3f61f3d84a9ad0877a7c1307f6
Jaime Martin <jaime.martincasado@usp.ceu.es>
2024-05-06 17:57:02 +0200

    commit jaime

--- src/obstetricianclinic/ifaces/LabStaffManager.java
+++ src/obstetricianclinic/ifaces/LabStaffManager.java
@@ -4,11 +4,6 @@ import obstetricianclinic.pojos.*;
 import java.util.List;
 
 public  interface LabStaffManager {
-<<<<<<< HEAD
 	public void addLabStaff(LaboratoryStaff laboratoryStaff);
 	public void updateLabStaff(LaboratoryStaff laboratoryStaff);
-=======
-	public void addLabStaff(LaboratoryStaff staff);
-	public void updateLabStaff(LaboratoryStaff staff);
->>>>>>> branch 'main' of https://github.com/oliviagallego/ObstetricianClinic
 }
