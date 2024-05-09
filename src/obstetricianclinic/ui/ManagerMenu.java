11f8267efc8dcdd9a701d787063d127e05fa0f80
Jaime Martin <jaime.martincasado@usp.ceu.es>
2024-05-09 18:16:33 +0200

    commit

--- src/obstetricianclinic/ui/ManagerMenu.java
+++ src/obstetricianclinic/ui/ManagerMenu.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 import obstetricianclinic.ifaces.UserManager;
-<<<<<<< HEAD
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,13 +17,7 @@ import obstetricianclinic.ifaces.*;
 import obstetricianclinic.pojos.*;
 import obstetricianclinic.jdbc.*;
 
-=======
-import obstetricianclinic.pojos.Role;
-import obstetricianclinic.pojos.User;
->>>>>>> branch 'main' of https://github.com/oliviagallego/ObstetricianClinic
-
 public class ManagerMenu {
-<<<<<<< HEAD
 	private static DiseaseManager diseaseMan;
 	private static LabReportManager labReportMan;
 	private static LabStaffManager labStaffMan;
@@ -49,27 +42,10 @@ public class ManagerMenu {
 					+ "\n 3. Change Password" 
 					+ "\n 0. Log out");
 			int option = Utilities.readInteger("Choose an option: ");
-=======
 	
 	private static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 	private static UserManager userMan;
-	
-	public static void menu( UserManager userMan) throws NumberFormatException, IOException{
-		System.out.print("Choose a username: ");
-		String username= r.readLine();
-		System.out.print("Password:");
-		String password= r.readLine();
-		System.out.println("Choose your role (type its name):");
-		List<Role> roles= userMan.getAllRoles();
-		System.out.println(roles);
-		String roleName= r.readLine();
-		Role r= userMan.getRole(roleName);
-		User u= new User(username, password, r);
-		
-	}
->>>>>>> branch 'main' of https://github.com/oliviagallego/ObstetricianClinic
-
-			switch(option) {
+		switch(option) {
 			case 1:{
 				System.out.println("\nRegistration of an obstetrician: ");
 				registerObstetrician();
